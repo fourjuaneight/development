83f671bcfd4da069d5466401fe51d2e40122c9bb
Juan Villela <juan@villela.co>
2021-11-22 17:20:55 -0500

    fix: Source rust env to use rustup.

--- util/cargo.sh
+++ util/cargo.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> .bashrc
-
 echo "installing rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> .bashrc
+source $HOME/.cargo/env
 rustup toolchain install nightly --allow-downgrade --profile minimal --component clippy
 
 echo "installing rust binaries"
