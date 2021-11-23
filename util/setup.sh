8c455ac900c3f7be228f06c0aaf9723cbeb504ef
Juan Villela <juan@villela.co>
2021-11-22 19:46:28 -0500

    feat: Add vim plugin install command.

--- util/setup.sh
+++ util/setup.sh
@@ -12,3 +12,4 @@ cp vscode-dracula.vsix ~/.vscode/extensions
 echo "installing vim plug"
 curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PluginInstall +qall
