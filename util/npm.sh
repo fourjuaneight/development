7e42ae4f505e959ebcefb19347df6fa5eea063d9
Juan Villela <juan@villela.co>
2021-11-23 12:39:51 -0500

    feat: Set Node version when installing npm packages.

--- util/npm.sh
+++ util/npm.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+echo 'export PATH=$HOME/.fnm:$PATH' >> .bashrc
+echo 'export PATH=$HOME/.fnm:$PATH' >> .profile
+eval "`fnm env`"
+
+echo "setting Node to latest LTS version"
+fnm use 16.13.0
+
 echo "installing global npm dependencies"
 npm i -g eslint fkill-cli glyphhanger prettier
