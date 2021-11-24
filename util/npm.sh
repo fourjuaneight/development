862d63c8406a7585a896bc276de929e61387bdf1
Juan Villela <juan@villela.co>
2021-11-23 19:27:09 -0500

    fix: Add full path to Node version manager.

--- util/npm.sh
+++ util/npm.sh
@@ -2,10 +2,10 @@
 
 echo 'export PATH=$HOME/.fnm:$PATH' >> .bashrc
 echo 'export PATH=$HOME/.fnm:$PATH' >> .profile
-eval "`fnm env`"
+eval "`~/.cargo/bin/fnm env`"
 
 echo "setting Node to latest LTS version"
-fnm use 16.13.0
+~/.cargo/bin/fnm use 16.13.0
 
 echo "installing global npm dependencies"
 npm i -g eslint fkill-cli glyphhanger prettier
