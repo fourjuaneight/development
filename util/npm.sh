9bba2705b47592aeae789499601c119f3fa1f2df
Juan Villela <juan@villela.co>
2021-11-30 13:38:49 -0500

    feat: Add serve npm dependency.

--- util/npm.sh
+++ util/npm.sh
@@ -9,4 +9,4 @@ echo "setting Node to latest LTS version"
 ~/.cargo/bin/fnm use 16.13.0
 
 echo "installing global npm dependencies"
-npm install -g eslint fkill-cli glyphhanger prettier
+npm install -g eslint fkill-cli glyphhanger prettier serve
