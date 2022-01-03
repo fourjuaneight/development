3a5e6ed698b6e328374b1482107eb62b1a03208c
Juan Villela <juan@villela.co>
2022-01-02 20:32:54 -0500

    fix: Add missing env variables.

--- util/npm.sh
+++ util/npm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 echo "setting Node to latest LTS version"
+eval "$(fnm env)"
+source ~/.bashrc
 ~/.cargo/bin/fnm install --lts
 ~/.cargo/bin/fnm use $(~/.cargo/bin/fnm list | ~/.cargo/bin/sd "\*\s" "" | ~/.cargo/bin/sd "\n" "" | ~/.cargo/bin/sd "%" "" | ~/.cargo/bin/sd ".*v(\d+\.\d+\.\d+)\slts-.*" '$1')
 
