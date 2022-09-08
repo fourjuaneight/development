2f795e9aea30a4f010bf185a2fda7398fe871bf7
Juan Villela <juan@villela.co>
2022-09-08 14:24:04 -0400

    chore: Update fnm util path.

--- util/npm.sh
+++ util/npm.sh
@@ -3,8 +3,8 @@
 echo "setting Node to latest LTS version"
 eval "$(fnm env)"
 source ~/.bashrc
-~/.cargo/bin/fnm install --lts
-~/.cargo/bin/fnm use $(~/.cargo/bin/fnm list | ~/.cargo/bin/sd "\*\s" "" | ~/.cargo/bin/sd "\n" "" | ~/.cargo/bin/sd "%" "" | ~/.cargo/bin/sd ".*v(\d+\.\d+\.\d+)\slts-.*" '$1')
+/home/node/.cargo/bin/fnm install --lts
+/home/node/.cargo/bin/fnm use $(~/.cargo/bin/fnm list | ~/.cargo/bin/sd "\*\s" "" | ~/.cargo/bin/sd "\n" "" | ~/.cargo/bin/sd "%" "" | ~/.cargo/bin/sd ".*v(\d+\.\d+\.\d+)\slts-.*" '$1')
 
 echo "installing global npm dependencies"
 npm install -g eslint fkill-cli glyphhanger prettier serve typescript
