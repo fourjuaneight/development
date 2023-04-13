4c878b4f21fdcb5c07fcdd570af2ccdab551252e
Juan Villela <juan@villela.co>
2023-04-13 12:35:04 -0400

    feat: Add Go language servers install script.

---
+++ util/go.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "installing Golang Language Server"
+go install github.com/nametake/golangci-lint-langserver@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
+go install github.com/cweill/gotests/gotests
+go install github.com/fatih/gomodifytags
+go install github.com/josharian/impl
+go install github.com/haya14busa/goplay/cmd/goplay
+go install github.com/go-delve/delve/cmd/dlv
+go install honnef.co/go/tools/cmd/staticcheck
+go install golang.org/x/tools/gopls
+go install github.com/erning/gorun@latest
