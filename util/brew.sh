6bf9a12f1de9da5505d683bfbdc26ce750ef0431
Juan Villela <juan@villela.co>
2024-04-10 17:54:26 -0400

    fix: Add correct binary paths and remove deprecated packages.

--- util/brew.sh
+++ util/brew.sh
@@ -14,33 +14,32 @@ brew tap helix-editor/helix
 brew install helix
 
 echo "installing packages"
-brew install ack \
-  charmbracelet/tap/mods \
-  cocoapods \
-  dive \
-  dog \
-  fx \
-  fzf \
-  gcc@5 \
-  gh \
-  glow \
-  go \
-  gum \
-  hugo \
-  knqyf263/pet/pet \
-  lindell/multi-gitter/multi-gitter \
-  lux \
-  neovim \
-  osv-scanner \
-  pnpm \
-  rclone \
-  restic \
-  syncthing \
-  tpm \
-  uv \
-  webp \
-  yq \
-  yt-dlp/taps/yt-dlp
+brew install ack
+brew install charmbracelet/tap/mods
+brew install cocoapods
+brew install dive
+brew install dog
+brew install fx
+brew install fzf
+brew install gcc@5
+brew install gh
+brew install glow
+brew install go
+brew install gum
+brew install hugo
+brew install knqyf263/pet/pet
+brew install lindell/multi-gitter/multi-gitter
+brew install lux
+brew install neovim
+brew install osv-scanner
+brew install pnpm
+brew install rclone
+brew install restic
+brew install syncthing
+brew install tpm
+brew install uv
+brew install webp
+brew install yq
 
 echo "cleaning up"
 brew cleanup
--- util/go.sh
+++ util/go.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 echo "installing Golang Language Server"
-/usr/local/go install github.com/nametake/golangci-lint-langserver@latest
-/usr/local/go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
-/usr/local/go install github.com/cweill/gotests/gotests
-/usr/local/go install github.com/fatih/gomodifytags
-/usr/local/go install github.com/josharian/impl
-/usr/local/go install github.com/haya14busa/goplay/cmd/goplay
-/usr/local/go install github.com/go-delve/delve/cmd/dlv
-/usr/local/go install honnef.co/go/tools/cmd/staticcheck
-/usr/local/go install golang.org/x/tools/gopls
-/usr/local/go install github.com/erning/gorun@latest
+/home/linuxbrew/.linuxbrew/bin/go install github.com/nametake/golangci-lint-langserver@latest
+/home/linuxbrew/.linuxbrew/bin/go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
+/home/linuxbrew/.linuxbrew/bin/go install github.com/cweill/gotests/gotests
+/home/linuxbrew/.linuxbrew/bin/go install github.com/fatih/gomodifytags
+/home/linuxbrew/.linuxbrew/bin/go install github.com/josharian/impl
+/home/linuxbrew/.linuxbrew/bin/go install github.com/haya14busa/goplay/cmd/goplay
+/home/linuxbrew/.linuxbrew/bin/go install github.com/go-delve/delve/cmd/dlv
+/home/linuxbrew/.linuxbrew/bin/go install honnef.co/go/tools/cmd/staticcheck
+/home/linuxbrew/.linuxbrew/bin/go install golang.org/x/tools/gopls
+/home/linuxbrew/.linuxbrew/bin/go install github.com/erning/gorun@latest
